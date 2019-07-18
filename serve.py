09e03660872b051837beddffa9f5c890d058a81e
ver09934 <ianvernooy@gmail.com>
2019-07-18 09:03:34 -0400

    Revert "Quick hack to use lslga_id instead of table index in URL"
    This reverts commit 7925647b543f68bb6d653eac40c5aaa79f53195e.

--- serve.py
+++ serve.py
@@ -79,8 +79,7 @@ def lslga():
     args = request.args
 
     if 'num' in args:
-        lslga_id = int(args['num'])
-        lslga_index = lslgautils.get_lslga_index(lslga_id)
+        lslga_index = int(args['num'])
     else:
         lslga_index = 0
     
@@ -138,10 +137,9 @@ def inspect():
             if lslgautils.test_footprint(rand_index):
                 break
 
-    rand_id = lslgautils.get_lslga_id(rand_index)
     galaxy_info = lslgautils.get_lslga_tablerow(rand_index)
 
-    return render_template("inspect.html", rand=rand_id, info=galaxy_info)
+    return render_template("inspect.html", rand=rand_index, info=galaxy_info)
 
 @app.route('/test')
 def toast():
@@ -172,8 +170,7 @@ def test(catalog_raw):
             if lslgautils.test_footprint(rand_index):
                 break
     
-    galaxy_id = lslgautils.get_lslga_id(rand_index)
-    return redirect(url_for('test2', catalog_raw=catalog_raw, galaxy_index=galaxy_id))
+    return redirect(url_for('test2', catalog_raw=catalog_raw, galaxy_index=rand_index))
 
 @app.route('/test/<string:catalog_raw>/<int:galaxy_index>')
 def test2(catalog_raw, galaxy_index):
@@ -182,8 +179,7 @@ def test2(catalog_raw, galaxy_index):
         error_msg = 'Catalog name not found.'
         abort(Response(render_template('500.html', error_msg=error_msg), 500))
 
-    actual_galaxy_index = lslgautils.get_lslga_index(galaxy_index)
-    galaxy_info = lslgautils.get_lslga_tablerow(actual_galaxy_index)
+    galaxy_info = lslgautils.get_lslga_tablerow(galaxy_index)
 
     viewer_link = (
         "http://legacysurvey.org/viewer"
