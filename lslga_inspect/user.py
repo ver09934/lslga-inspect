0ecf95128caa484e7138e8aebf61446775fc553c
ver09934 <ianvernooy@gmail.com>
2019-07-23 15:42:31 -0400

    Temporarily condense user page

--- lslga_inspect/user.py
+++ lslga_inspect/user.py
@@ -11,13 +11,11 @@ def userindex():
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
-
         if not username:
             error = 'Username is required.'
         elif not password:
@@ -26,7 +24,6 @@ def register():
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
-
         if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
@@ -34,9 +31,7 @@ def register():
             )
             db.commit()
             return redirect(url_for('user.login'))
-
         flash(error)
-
     return render_template('user.html', action="register")
 
 @bp.route('/login', methods=('GET', 'POST'))
@@ -49,19 +44,15 @@ def login():
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
-
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
-
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('inspect.index'))
-
         flash(error)
-
     return render_template('user.html', action="login")
 
 @bp.before_app_request
